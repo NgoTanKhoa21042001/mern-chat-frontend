1e438456465771f549a9b0be329ab28351f2a2fd
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-08-26 10:39:35 +0700

    Hoàn thiện giao diện phần chat, login, signup. Hoàn thiện phần upload hình ảnh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
-
+import Navigation from "./components/Navigation";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
